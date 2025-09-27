92bb745e0701356cb08deb4b99b05fb90cf4a8d7
kbburk-TSF <kevin@temporalinertia.com>
2025-09-27 15:27:04 -0500

    Update plasmic-host.jsx

--- pages/plasmic-host.jsx
+++ pages/plasmic-host.jsx
@@ -4,14 +4,15 @@ import * as React from 'react';
 import { PlasmicCanvasHost, registerComponent } from '@plasmicapp/react-web/lib/host';
 import ThreeChartsBlock from '../components/ThreeChartsBlock';
 
-// Register your code component exactly once (no duplicate imports)
 registerComponent(ThreeChartsBlock, {
   name: 'ThreeChartsBlock',
+  importPath: 'components/ThreeChartsBlock', // <- required by Plasmic
+  defaultExport: true,
   props: {
     rows: 'object',
     width: { type: 'number', defaultValue: 1200 },
-    height: { type: 'number', defaultValue: 260 },
-  },
+    height: { type: 'number', defaultValue: 260 }
+  }
 });
 
 export default function PlasmicHost() {
