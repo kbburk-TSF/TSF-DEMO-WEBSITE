d3a25cf40119a097481a7902de901cdc08e16dc1
kbburk-TSF <kevin@temporalinertia.com>
2025-09-27 17:48:50 -0500

    fix path

--- pages/plasmic-host.jsx
+++ pages/plasmic-host.jsx
@@ -1,8 +1,8 @@
-// plasmic-host.jsx
+// pages/plasmic-host.jsx
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
 import { PlasmicCanvasHost, registerComponent } from "@plasmicapp/react-web/lib/host";
-import TwoChartEngine from "./components/TwoChartEngine.jsx";
+import TwoChartEngine from "../components/TwoChartEngine.jsx";
 
 registerComponent(TwoChartEngine, {
   name: "TwoChartEngine",
@@ -13,10 +13,12 @@ function App() {
   return <PlasmicCanvasHost />;
 }
 
-const rootEl = document.getElementById("root");
-if (rootEl) {
-  const root = ReactDOM.createRoot(rootEl);
-  root.render(<App />);
+if (typeof document !== "undefined") {
+  const rootEl = document.getElementById("root");
+  if (rootEl) {
+    const root = ReactDOM.createRoot(rootEl);
+    root.render(<App />);
+  }
 }
 
 export default App;
