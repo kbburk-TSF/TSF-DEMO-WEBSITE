6b405f4f8e1ec401e710606265d92319ec96b226
kbburk-TSF <kevin@temporalinertia.com>
2025-09-27 15:22:52 -0500

    Update plasmic-host.jsx

--- pages/plasmic-host.jsx
+++ pages/plasmic-host.jsx
@@ -1,20 +1,18 @@
-import { registerComponent } from "@plasmicapp/host";
-import ThreeChartsBlock from "../components/ThreeChartsBlock";
-registerComponent(ThreeChartsBlock, { name: "ThreeChartsBlock" });
-
-
+// pages/plasmic-host.jsx
 
 import * as React from 'react';
 import { PlasmicCanvasHost, registerComponent } from '@plasmicapp/react-web/lib/host';
+import ThreeChartsBlock from '../components/ThreeChartsBlock';
 
-// You can register any code components that you want to use here; see
-// https://docs.plasmic.app/learn/code-components-ref/
-// And configure your Plasmic project to use the host url pointing at
-// the /plasmic-host page of your nextjs app (for example,
-// http://localhost:3000/plasmic-host).  See
-// https://docs.plasmic.app/learn/app-hosting/#set-a-plasmic-project-to-use-your-app-host
-
-// registerComponent(...)
+// Register your code component exactly once (no duplicate imports)
+registerComponent(ThreeChartsBlock, {
+  name: 'ThreeChartsBlock',
+  props: {
+    rows: 'object',
+    width: { type: 'number', defaultValue: 1200 },
+    height: { type: 'number', defaultValue: 260 },
+  },
+});
 
 export default function PlasmicHost() {
   return <PlasmicCanvasHost />;
