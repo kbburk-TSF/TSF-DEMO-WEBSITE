91b3e17ae4da946e482deef2cb5fd73036d528f1
kbburk-TSF <kevin@temporalinertia.com>
2025-09-27 16:55:50 -0500

    Update plasmic-host.jsx

--- pages/plasmic-host.jsx
+++ pages/plasmic-host.jsx
@@ -5,8 +5,19 @@ import { PlasmicCanvasHost, registerComponent } from '@plasmicapp/react-web/lib/
 import DashboardTab from '../components/DashboardTab';
 import DashboardTab2 from '../components/DashboardTab2';
 
-registerComponent(DashboardTab, { name: "DashboardTab" });
-registerComponent(DashboardTab2, { name: "DashboardTab2" });
+registerComponent(DashboardTab, {
+  name: "DashboardTab",
+  importPath: "../components/DashboardTab",
+  importName: "default",
+  props: {}
+});
+
+registerComponent(DashboardTab2, {
+  name: "DashboardTab2",
+  importPath: "../components/DashboardTab2",
+  importName: "default",
+  props: {}
+});
 
 export default function PlasmicHost() {
   return <PlasmicCanvasHost />;
