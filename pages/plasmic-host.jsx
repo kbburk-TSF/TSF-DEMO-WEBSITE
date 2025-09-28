a300f0018dbbced4502d67976b54241b06b46705
kbburk-TSF <kevin@temporalinertia.com>
2025-09-27 20:41:32 -0500

    iframe

--- pages/plasmic-host.jsx
+++ pages/plasmic-host.jsx
@@ -1,13 +1,48 @@
 // pages/plasmic-host.jsx
 import * as React from "react";
 import { PlasmicCanvasHost, registerComponent } from "@plasmicapp/react-web/lib/host";
-import TwoChartsPlasmic from "../components/TwoChartsPlasmic.jsx";
+import IframeEmbed from "../components/IframeEmbed.jsx";
 
-registerComponent(TwoChartsPlasmic, {
-  name: "TwoChartsPlasmic",
-  importPath: "./components/TwoChartsPlasmic",
+registerComponent(IframeEmbed, {
+  name: "IframeEmbed",
+  importPath: "./components/IframeEmbed",
   isDefaultExport: true,
-  props: {},
+  props: {
+    src: {
+      type: "string",
+      displayName: "URL",
+      defaultValue: "https://tsfdemo.com/features"
+    },
+    minHeight: {
+      type: "number",
+      displayName: "Min height (px)",
+      defaultValue: 1200
+    },
+    loading: {
+      type: "choice",
+      options: ["eager", "lazy"],
+      defaultValue: "eager"
+    },
+    allowFullscreen: {
+      type: "boolean",
+      displayName: "Allow fullscreen",
+      defaultValue: true
+    },
+    referrerPolicy: {
+      type: "choice",
+      options: [
+        "no-referrer",
+        "no-referrer-when-downgrade",
+        "origin",
+        "origin-when-cross-origin",
+        "same-origin",
+        "strict-origin",
+        "strict-origin-when-cross-origin",
+        "unsafe-url"
+      ],
+      defaultValue: "no-referrer"
+    }
+  },
 });
 
 export default function PlasmicHost() {
